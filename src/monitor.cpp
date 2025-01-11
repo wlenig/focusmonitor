5eae21a4165013700cb94cb240e5c7f4f8a5c7e9
smore007 <30681316+smore007@users.noreply.github.com>
2025-01-11 15:55:57 -0500

    Cleaned up output

--- src/monitor.cpp
+++ src/monitor.cpp
@@ -1,16 +1,24 @@
 #include <Windows.h>
 #include <stdexcept>
 #include <iostream>
+#include <iomanip>
 #include <Psapi.h>
+#include <Shlwapi.h>
 
 struct FocusInfo {
+    HWND window;
     DWORD process_id;
-    char process_name[MAX_PATH];
+    char executable_file[MAX_PATH];
+    char window_name[MAX_PATH];
+    const char* executable_name;
 };
 
 auto get_info(HWND hwnd)
 {
     FocusInfo info{};
+    info.executable_file[0] = '\0';
+    info.window_name[0] = '\0';
+    info.executable_name = "";
 
     if (!GetWindowThreadProcessId(hwnd, &info.process_id)) {
         throw std::runtime_error("Failed to get process ID");
@@ -21,25 +29,41 @@ auto get_info(HWND hwnd)
         throw std::runtime_error("Failed to open process");
     }
 
-    if(!GetModuleFileNameExA(process, NULL, info.process_name, sizeof(info.process_name))) {
+    if (!GetModuleFileNameExA(process, NULL, info.executable_file, sizeof(info.executable_file))) {
         CloseHandle(process);
-        throw std::runtime_error("Failed to get file name");
+        throw std::runtime_error("Failed to get executable name");
     }
+
+    // Get short file name
+    info.executable_name = PathFindFileNameA(info.executable_file);
+
+    // Get window title name. Note: not all windows have title names, so we don't check return
+    GetWindowTextA(hwnd, info.window_name, sizeof(info.window_name));
 
     CloseHandle(process);
     return info;
 }
 
+auto get_current_time()
+{
+    auto now = std::time(nullptr);
+    auto local = std::localtime(&now);
+
+    char buffer[8 + 1]; // strlen("HH:MM:SS") = 8
+    std::strftime(buffer, sizeof(buffer), "%H:%M:%S", local);
+
+    return std::string(buffer);
+}
+
 auto log_info(FocusInfo info)
 {
-    // Prevent duplicates, as the event is often triggered multiple times per focus change
-    static DWORD last_pid = 0;
-    if (info.process_id == last_pid) {
-        return;
-    }
-    last_pid = info.process_id;
+    auto time_str = get_current_time();
 
-    std::cout << info.process_id << " : " << info.process_name << std::endl;
+    std::cout << "[" << time_str << "] " << info.executable_name << std::endl;
+    std::cout << std::setw(16) << std::right << "Process ID : " << info.process_id << std::endl;
+    std::cout << std::setw(16) << std::right << "Window title : " << info.window_name << std::endl;
+    std::cout << std::setw(16) << std::right << "Executable : " << info.executable_file << std::endl;
+    std::cout << std::setw(16) << std::right << "Handle : " << info.window << std::endl;
 }
 
 void handle_event(
@@ -66,21 +90,26 @@ void message_loop()
 
 int main()
 {
-    auto hook = SetWinEventHook(
-        EVENT_OBJECT_FOCUS, EVENT_OBJECT_FOCUS,
-        NULL,
-        handle_event,
-        0,
-        0,
-        WINEVENT_OUTOFCONTEXT
-    );
+    try {
 
-    if (!hook) {
-        throw std::runtime_error("Failed to set event hook");
+        auto hook = SetWinEventHook(
+            EVENT_OBJECT_FOCUS, EVENT_OBJECT_FOCUS,
+            NULL,
+            handle_event,
+            0,
+            0,
+            WINEVENT_OUTOFCONTEXT
+        );
+
+        if (!hook) {
+            throw std::runtime_error("Failed to set event hook");
+        }
+
+        message_loop();
+        UnhookWinEvent(hook);
+
+        return 0;
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
     }
-
-    message_loop();
-    UnhookWinEvent(hook);
-
-    return 0;
 }
