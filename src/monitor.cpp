bf0227bffab2a52ae1571423c20d7edcfccc8208
smore007 <30681316+smore007@users.noreply.github.com>
2025-01-11 15:11:07 -0500

    Added more info, such as process name, to the output

--- src/monitor.cpp
+++ src/monitor.cpp
@@ -1,17 +1,58 @@
 #include <Windows.h>
 #include <stdexcept>
 #include <iostream>
+#include <Psapi.h>
+
+struct FocusInfo {
+    DWORD process_id;
+    char process_name[MAX_PATH];
+};
+
+auto get_info(HWND hwnd)
+{
+    FocusInfo info{};
+
+    if (!GetWindowThreadProcessId(hwnd, &info.process_id)) {
+        throw std::runtime_error("Failed to get process ID");
+    }
+
+    auto process = OpenProcess(PROCESS_QUERY_INFORMATION, FALSE, info.process_id);
+    if (process == NULL) {
+        throw std::runtime_error("Failed to open process");
+    }
+
+    if(!GetModuleFileNameExA(process, NULL, info.process_name, sizeof(info.process_name))) {
+        CloseHandle(process);
+        throw std::runtime_error("Failed to get file name");
+    }
+
+    CloseHandle(process);
+    return info;
+}
+
+auto log_info(FocusInfo info)
+{
+    // Prevent duplicates, as the event is often triggered multiple times per focus change
+    static DWORD last_pid = 0;
+    if (info.process_id == last_pid) {
+        return;
+    }
+    last_pid = info.process_id;
+
+    std::cout << info.process_id << " : " << info.process_name << std::endl;
+}
 
 void handle_event(
     HWINEVENTHOOK hWinEventHook,
     DWORD event,
-    HWND hwnd,
+    HWND hwnd, // handle to window being focused
     LONG idObject,
     LONG idChild,
     DWORD idEventThread,
     DWORD dwmsEventTime
 ) {
-    std::cout << hwnd << std::endl;
+    auto info = get_info(hwnd);
+    log_info(info);
 }
 
 void message_loop()
@@ -40,4 +81,6 @@ int main()
 
     message_loop();
     UnhookWinEvent(hook);
+
+    return 0;
 }
